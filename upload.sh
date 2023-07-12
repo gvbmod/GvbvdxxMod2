72b65e711bb6a8198d259d6fccd382b7fc537001
gvbvdxx-test <jgevans@students.wcpss.net>
2023-07-11 21:21:04 -0400

    Uploading stuff

--- upload.sh
+++ upload.sh
@@ -1,6 +1,6 @@
 git init
-git add ./*
-git commit -m "Upload From Git Bash"
+git add .
+git commit -m "Uploading stuff"
 git branch -M main
 git remote add origin https://github.com/gvbvdxx-test/GvbvdxxMod2.git
-git push -u origin main
+git push -f origin main
