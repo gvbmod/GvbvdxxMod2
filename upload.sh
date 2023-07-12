fcfc9004645b68ea1f4582a2ff59de354ccdaf31
gvbvdxx-test <jgevans@students.wcpss.net>
2023-07-11 21:15:40 -0400

    Upload From Git Bash

---
+++ upload.sh
@@ -0,0 +1,13 @@
+git init
+git add .
+git commit -m "Upload From Git Bash"
+git branch -M main
+git remote add origin https://github.com/gvbvdxx-test/GvbvdxxMod2.git
+git push -u origin main
+cd scratch-gui/build
+git init
+git add .
+git commit -m "Upload From Git Bash"
+git branch -M website
+git remote add origin https://github.com/gvbvdxx-test/GvbvdxxMod2.git
+git push -u origin website
