bfe29d62ab092dc3dcab063fed813cee28663682
gvbvdxx-test <jgevans@students.wcpss.net>
2023-07-11 21:19:50 -0400

    Upload From Git Bash

--- upload-website.sh
+++ upload-website.sh
@@ -1,6 +1,6 @@
 git init
-git add ./*
+git add .
 git commit -m "Upload From Git Bash"
-git branch -M main
+git branch -M website
 git remote add origin https://github.com/gvbvdxx-test/GvbvdxxMod2.git
-git push -u origin main
+git push -u origin website
