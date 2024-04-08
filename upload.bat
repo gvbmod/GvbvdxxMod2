3f7aa8231b1742fe3301cfc5d738c5ad34d8ffef
gvbvdxx <gvbvdxx@outlook.com>
2024-04-08 06:53:25 -0400

    Upload main source code.

--- upload.bat
+++ upload.bat
@@ -4,4 +4,4 @@ git add .
 git commit -m "Upload main source code."
 git branch -M main
 git remote add origin https://github.com/gvbmod/GvbvdxxMod2.git
-git push --no-verify origin main
+git push -f --no-verify origin main
